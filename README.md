52f3d9705e5c972fd054d6d6284809c61ba5a5c8
Carlos Fernando Avila Gratz <cafe@q1software.com>
2013-06-24 16:51:31 -0300

    version 0.01

--- README.md
+++ README.md
@@ -2,3 +2,54 @@ jquery-smooth-scroll
 ====================
 
 jQuery Smooth Scrolling Anchors Plugin
+
+
+# Usage
+
+Using <a href="#target-id">:
+
+    <a href="#foo">Scroll to foo smoothly.</a>
+
+    <!-- the scroll target -->
+    <div id="foo"> ... </div>
+
+
+    <!-- initialize -->
+    $(function(){
+        $('a[href^=#']).smoothScroll({
+            // example using default options
+            duration: 600,
+            easing: 'swing'
+        });
+    })
+
+Using any element:
+
+    <!-- data-scroll-to accepts any jQuery-compatible selector -->
+    <h1 data-scroll-to=".foo">Scroll to .foo smoothly.</h1>
+
+    <!-- the scroll target -->
+    <div class="foo"> ... </div>
+
+    <!-- initialize -->
+    $(function(){
+        $('h1').smoothScroll();
+    })
+
+# Config Options
+
+* duration - the scroll animation duration
+* easing - the scroll animation easing
+
+# Config Attributes
+
+You can add config attributes to the clickable elements.
+
+* data-scroll-duration
+* data-scroll-easing
+* data-scroll-offset
+
+
+
+
+
---
+++ smooth_scroll.js
@@ -0,0 +1,72 @@
+// the semi-colon before function invocation is a safety net against concatenated
+// scripts and/or other plugins which may not be closed properly.
+;(function ( $, window, document, undefined ) {
+
+    // Create the defaults once
+    var pluginName = "Q1SmoothScroll",
+        defaults = {
+            duration: 600,
+            easing: 'swing'
+        };
+
+    // The actual plugin constructor
+    function Q1SmoothScroll( element, options ) {
+
+        this.element = element;
+        this.options = $.extend( {}, defaults, options );
+
+        this._defaults = defaults;
+        this._name = pluginName;
+        this._version = 0.01;
+
+        this.init();
+    }
+
+    Q1SmoothScroll.prototype = {
+
+        init: function() {
+            var me = this,
+                options = me.options,
+                el = $(me.element),
+                duration = el.data('scroll-duration') || options.duration,
+                easing = el.data('scroll-easing') || options.easing,
+                scrollOffset = el.data('scroll-offset') ? parseInt(el.data('scroll-offset'), 10) : 0,
+                targetSelector = el.attr('href') || el.data('scroll-to') || '',
+                targetEl;
+
+            console.log('easing:', easing);
+
+            if ( targetSelector == '' ) {
+                return console.error('[%s] invalid "href" or "data-scroll-to" attribute (%s). Use href="#foo" or data-scroll-to=".foo" (a css selector).', pluginName, targetSelector);
+            }
+
+            targetEl = $(targetSelector).first();
+
+            if (!targetEl) {
+                return console.error("[%s] can't find element '%s'", pluginName, targetSelector);
+            }
+
+            el.click(function(evt){
+                evt.preventDefault();
+
+                var targetOffset = targetEl.offset().top + scrollOffset;
+
+                $('html, body').animate({ scrollTop: targetOffset }, duration, easing);
+            });
+
+
+        }
+
+    };
+
+    // A really lightweight plugin wrapper around the constructor,
+    // preventing against multiple instantiations
+    $.fn['smoothScroll'] = function ( options ) {
+        return this.each(function () {
+            if (!$.data(this, "plugin_" + pluginName)) {
+                $.data(this, "plugin_" + pluginName, new Q1SmoothScroll( this, options ));
+            }
+        });
+    };
+
+})( jQuery, window, document );
